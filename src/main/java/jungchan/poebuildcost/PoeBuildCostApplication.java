51d5958d3f5161235fe70624cff14eeb918aead6
jungchanSon <rnrmfjc@gmail.com>
2024-04-24 21:04:15 +0900

    feat: add profiles

--- src/main/java/jungchan/poebuildcost/PoeBuildCostApplication.java
+++ src/main/java/jungchan/poebuildcost/PoeBuildCostApplication.java
@@ -2,12 +2,20 @@ package jungchan.poebuildcost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class PoeBuildCostApplication {
 
+	public static final String PROFILE_LOCATION= "spring.config.location="
+			+ "classpath:application.yaml,"
+			+ "classpath:profile.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(PoeBuildCostApplication.class, args);
+		new SpringApplicationBuilder(PoeBuildCostApplication.class)
+				.properties(PROFILE_LOCATION)
+				.run(args);
+//		SpringApplication.run(PoeBuildCostApplication.class, args);
 	}
 }
---
+++ src/main/java/jungchan/poebuildcost/controller/ProfileController.java
@@ -0,0 +1,33 @@
+package jungchan.poebuildcost.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ProfileController {
+
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String getProfile(){
+        String s = Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+
+        System.out.println("s = " + s);
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
+}
